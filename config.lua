ecdd71c6c58bad8461e3e684c94dad8f210a2da8
DaCyclops <abcyclops@gmail.com>
2015-09-12 14:35:27 +1000

    Adjusted down default polling rates

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 -- Times per second (max 60) that robotic combinators will update values.
---  Each combinator type has seperate figures for: 
+--  Each combinator type has separate figures for: 
 --    - Fast Update Processes (main values of provided stats)
---    - Slow Update Processes (calulating stats that could cause lag)
+--    - Slow Update Processes (calculating stats that could cause lag)
 -- Feel free to tweak these figures 
 
-rc_polling_rate_network = 20
-rc_polling_rate_network_slow = 10
-rc_polling_rate_personal = 20
+rc_polling_rate_network = 15
+rc_polling_rate_network_slow = 4
+rc_polling_rate_personal = 15
 rc_polling_rate_personal_slow = 5
-rc_polling_rate_local = 30
+rc_polling_rate_local = 20
 rc_polling_rate_local_slow = 10
